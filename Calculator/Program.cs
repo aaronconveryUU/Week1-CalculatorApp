e5b1811c4fb0cb7d69e68800c0c1db8175c9ad32
AC <convery-a8@ulster.ac.uk>
2025-09-29 16:44:08 +0100

    Added calculation choices

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,22 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int firstnumber = 5;
+CalculatorApp();
 
-int secondnumber = 20;
+void CalculatorApp()
+{
+    int firstnumber = 0;
+    int secondnumber = 0;
+    int result = 0;
+    char calctype = 'A';
 
-int result = firstnumber + secondnumber;
+    Console.WriteLine("Type in the first number followed by the enter key");
+    firstnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"The addition of both numbers is {result}");
-Console.ReadKey();
+    Console.WriteLine("Type in the second number followed by the enter key");
+    secondnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Type in the first number followed by the enter key");
-firstnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Type in the second number followed by the enter key");
-secondnumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Choose an item from the following list:\n1-Add\n2-Subtract\n3-Divide\n4-Multiply");
+    calctype = Convert.ToChar(Console.ReadLine());
 
-result = firstnumber + secondnumber;
-Console.WriteLine($"Adding {firstnumber} and {secondnumber} gives the answer {result}");
-Console.ReadKey();
+    switch (calctype)
+    {
+        case '1':
+            result = firstnumber + secondnumber;
+            break;
+        case '2':
+            result = firstnumber - secondnumber;
+            break;
+        case '3':
+            result = firstnumber / secondnumber;
+            break;
+        case '4':
+            result = firstnumber * secondnumber;
+            break;
 
-Console.WriteLine("Please Work");
+    }    
+
+
+    
+    ;
+    Console.WriteLine($"The result is {result}");
+    Console.ReadKey();
+
+    Console.WriteLine("Please Work");
+
+}
+
+
