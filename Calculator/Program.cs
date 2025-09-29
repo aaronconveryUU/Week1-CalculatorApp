7fb5c4f43866cb6ab5fd8a0fb63c5393f613b24b
AC <convery-a8@ulster.ac.uk>
2025-09-29 16:27:17 +0100

    Added 2 calculations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Please Work");
+
+int firstnumber = 5;
+
+int secondnumber = 20;
+
+int result = firstnumber + secondnumber;
+
+Console.WriteLine($"The addition of both numbers is {result}");
+Console.ReadKey();
+
+Console.WriteLine("Type in the first number followed by the enter key");
+firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by the enter key");
+secondnumber = Convert.ToInt32(Console.ReadLine());
+
+result = firstnumber + secondnumber;
+Console.WriteLine($"Adding {firstnumber} and {secondnumber} gives the answer {result}");
+Console.ReadKey();
